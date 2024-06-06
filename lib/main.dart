3b95efa68fcafbc3c01fe10d030242f108557df1
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2024-06-07 00:18:21 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'package:regular_tasks/tasks/carousel%20slider/carousel_slider_example.dart';
+import 'package:regular_tasks/tasks/audio%20video%20player/audio_video_player_exmaple.dart';
+import 'package:regular_tasks/tasks/audio%20video%20player/home%20page/provider/audio_video_provider.dart';
 import 'package:regular_tasks/tasks/carousel%20slider/home%20page/provider/carousel_provider.dart';
+
+// CarouselSliderExample()
+// AudioVideoPlayerExample()
+
 
 void main(){
   runApp(MultiProvider(
 
       providers: [
         ChangeNotifierProvider(create: (context) => CarouselProvider()),
+        ChangeNotifierProvider(create: (context) => AudioVideoProvider()),
       ],
-      child: const CarouselSliderExample()));
+      child: const AudioVideoPlayerExample()));
 }
 
 
